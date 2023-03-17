c719f39c7f177e3174626fe61815047b03abb226
madiepev <madiepev@microsoft.com>
2023-03-17 12:02:02 +0100

    update action

--- src/model/train.py
+++ src/model/train.py
@@ -1,11 +1,11 @@
 # Import libraries
-
 import argparse
 import glob
 import os
+import mlflow
 
 import pandas as pd
-
+from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 
 
